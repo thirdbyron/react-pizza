081b9e0262f981d62b798d6fcdaad6fa275f7470
thirdbyron <rgnatovsky@gmail.com>
2022-09-14 14:03:44 +0300

    Включает переключение категорий

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,29 @@
+import { useEffect, useState, useRef } from 'react';
+
 export const Categories = () => {
+  const ref = useRef();
+  const [activeType, setActiveType] = useState(ref.current);
+
+  useEffect(() => {
+    ref.current.className = 'active';
+  }, [activeType]);
+
+  const onChangeCategorie = (evt) => {
+    ref.current.className = '';
+    ref.current = evt.target;
+    setActiveType(ref.current);
+  };
+
   return (
-    <div class='categories'>
-    <ul>
-      <li class='active'>Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
-    </ul>
-  </div>
+    <div className='categories'>
+      <ul onClick={onChangeCategorie}>
+        <li ref={ref}>Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
   );
-}
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
+import mainLogo from '../assets/img/pizza-logo.svg'
+
 export const Header = () => {
   return (
-    <div class='header'>
-      <div class='container'>
-        <div class='header__logo'>
+    <div className='header'>
+      <div className='container'>
+        <div className='header__logo'>
           <img
             width='38'
-            src='./img/pizza-logo.svg'
+            src={mainLogo}
             alt='Pizza logo'
           />
           <div>
@@ -13,13 +15,13 @@ export const Header = () => {
             <p>самая вкусная пицца во вселенной</p>
           </div>
         </div>
-        <div class='header__cart'>
+        <div className='header__cart'>
           <a
             href='/cart.html'
-            class='button button--cart'
+            className='button button--cart'
           >
             <span>520 ₽</span>
-            <div class='button__delimiter'></div>
+            <div className='button__delimiter'></div>
             <svg
               width='18'
               height='18'
